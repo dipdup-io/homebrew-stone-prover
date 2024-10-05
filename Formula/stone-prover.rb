6918a3e7f153292508422750bc9e895321f86506
Anton Rudenko <mrroudyk09@gmail.com.com>
2024-10-05 18:57:38 +0100

    add version

--- Formula/stone-prover.rb
+++ Formula/stone-prover.rb
@@ -3,18 +3,15 @@ class StoneProver < Formula
   homepage "https://github.com/baking-bad/stone-prover"
   license "MIT"
 
-  # Эти поля будут заменены workflow через sed
   url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.9/stone-prover-1.0.9-macos-arm64.tar.gz"
   sha256 "8ed584e4834dc77593d107b1413688deba73814f3aacf863672aea2daadfdb0d"
+  version "1.0.9"
 
   depends_on "gmp"
   depends_on "python@3.9"
 
   def install
-    # Распаковать архив в libexec
     libexec.install Dir["*"]
-
-    # Установить бинарные файлы в bin без суффиксов архитектуры
     bin.install_symlink libexec/"cpu_air_prover-#{arch_suffix}" => "cpu_air_prover"
     bin.install_symlink libexec/"cpu_air_verifier-#{arch_suffix}" => "cpu_air_verifier"
   end
@@ -24,12 +21,7 @@ class StoneProver < Formula
     assert_match version.to_s, shell_output("#{bin}/cpu_air_verifier --version")
   end
 
-  # Метод для определения суффикса архитектуры, если необходимо
   def arch_suffix
-    if Hardware::CPU.arm?
-      "arm64"
-    else
-      "x86_64"
-    end
+    Hardware::CPU.arm? ? "arm64" : "x86_64"
   end
 end
