d33b6a721a6fc2c5ac895518e1aeac36faee3e6c
Anton Rudenko <mrroudyk09@gmail.com.com>
2024-10-06 20:33:17 +0100

    Refactor

--- Formula/stone-prover.rb
+++ Formula/stone-prover.rb
@@ -9,18 +9,19 @@ class StoneProver < Formula
 
   bottle do
     root_url "https://github.com/MrRoudyk/stone-packaging/releases/download/v#{version}"
-    sha256 cellar: :any_skip_relocation, sonoma: "${SHA256_x86_64}"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "${SHA256_arm64}"
+    sha256 cellar: :any_skip_relocation, sonoma: ENV["SHA256_x86_64"]
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: ENV["SHA256_arm64"]
   end
 
   def install
-    arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
+    arch = Hardware::CPU.arch.to_s
     bin.install "cpu_air_prover-#{arch}" => "cpu_air_prover"
     bin.install "cpu_air_verifier-#{arch}" => "cpu_air_verifier"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/cpu_air_prover --version")
-    assert_match version.to_s, shell_output("#{bin}/cpu_air_verifier --version")
+    %w[cpu_air_prover cpu_air_verifier].each do |bin_name|
+      assert_match version.to_s, shell_output("#{bin}/#{bin_name} --version")
+    end
   end
 end
