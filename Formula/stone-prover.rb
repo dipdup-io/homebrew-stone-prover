79d2081ff0c2eaa1a050da31fcadfe37c91052e3
Anton Rudenko <mrroudyk09@gmail.com.com>
2024-10-06 13:38:03 +0100

    add bottle

--- Formula/stone-prover.rb
+++ Formula/stone-prover.rb
@@ -2,25 +2,25 @@ class StoneProver < Formula
   desc "Stone Prover"
   homepage "https://github.com/baking-bad/stone-prover"
   license "MIT"
-
-  url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.9/stone-prover-1.0.9-macos-arm64.tar.gz"
-  sha256 "8ed584e4834dc77593d107b1413688deba73814f3aacf863672aea2daadfdb0d"
   version "1.0.9"
 
   depends_on "gmp"
   depends_on "python@3.9"
 
+  bottle do
+    root_url "https://github.com/MrRoudyk/stone-packaging/releases/download/v#{version}"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "SHA256_FOR_ARM64"
+    sha256 cellar: :any_skip_relocation, monterey: "SHA256_FOR_X86_64"
+  end
+
   def install
-    bin.install "cpu_air_prover-#{arch_suffix}" => "cpu_air_prover"
-    bin.install "cpu_air_verifier-#{arch_suffix}" => "cpu_air_verifier"
+    arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
+    bin.install "cpu_air_prover-#{arch}" => "cpu_air_prover"
+    bin.install "cpu_air_verifier-#{arch}" => "cpu_air_verifier"
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/cpu_air_prover --version")
     assert_match version.to_s, shell_output("#{bin}/cpu_air_verifier --version")
-  end
-
-  def arch_suffix
-    Hardware::CPU.arm? ? "arm64" : "x86_64"
   end
 end
