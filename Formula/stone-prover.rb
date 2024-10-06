ae63cd36074a1687c38594e50e3bce74c6a47703
Anton Rudenko <mrroudyk09@gmail.com.com>
2024-10-07 00:12:58 +0100

    Delete bottle

--- Formula/stone-prover.rb
+++ Formula/stone-prover.rb
@@ -7,10 +7,12 @@ class StoneProver < Formula
   depends_on "gmp"
   depends_on "python@3.9"
 
-  bottle do
-    root_url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.17"
-    sha256 cellar: :any_skip_relocation, sonoma: "aaa45b26b6389295789903577b7a0e580ed2305e5757b8acd6a8e65cdb844a4d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "5cd83f442f77238540e04d90f9e58715414a2c6595fb0213d8ec3d03b0561958"
+  if Hardware::CPU.arm?
+    url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.17/cpu_air_prover-arm64.tar.gz"
+    sha256 "5cd83f442f77238540e04d90f9e58715414a2c6595fb0213d8ec3d03b0561958"
+  else
+    url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.17/cpu_air_prover-x86_64.tar.gz"
+    sha256 "aaa45b26b6389295789903577b7a0e580ed2305e5757b8acd6a8e65cdb844a4d"
   end
 
   def install
