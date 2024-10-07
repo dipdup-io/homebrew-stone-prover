6d7302b4198495f66ac3e68ffed2c73d6aecc69c
Anton Rudenko <mrroudyk09@gmail.com.com>
2024-10-07 16:14:15 +0100

    Only macOS arm64

--- Formula/stone-prover.rb
+++ Formula/stone-prover.rb
@@ -3,22 +3,15 @@ class StoneProver < Formula
   homepage "https://github.com/baking-bad/stone-prover"
   license "MIT"
   version "1.0.19"
-
+  url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.19/stone-prover-macos-arm64.tar.gz"
+  sha256 "9be65dddb491f3b4ce67948a4e98c676f318c46e7e9442993290ef3e0afa9782"
+  
   depends_on "gmp"
   depends_on "python@3.9"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.19/stone-prover-macos-arm64.tar.gz"
-    sha256 "9be65dddb491f3b4ce67948a4e98c676f318c46e7e9442993290ef3e0afa9782"
-  else
-    url "https://github.com/MrRoudyk/stone-packaging/releases/download/v1.0.19/stone-prover-macos-x86_64.tar.gz"
-    sha256 "2f36bf840ee00ea0c775183625ddb982ea8b542635cb38079536747e4398dced"
-  end
-
   def install
-    arch = Hardware::CPU.arch
-    bin.install "cpu_air_prover-#{arch}" => "cpu_air_prover"
-    bin.install "cpu_air_verifier-#{arch}" => "cpu_air_verifier"
+    bin.install "cpu_air_prover-arm64" => "cpu_air_prover"
+    bin.install "cpu_air_verifier-arm64" => "cpu_air_verifier"
   end
 
   test do
